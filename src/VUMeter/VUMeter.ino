a9624d4ee52c9ce2929e41b7ec119ad11539d620
Shelby Jueden <shelby@jueden.org>
2018-03-26 22:44:15 -0700

    Getting something

--- src/VUMeter/VUMeter.ino
+++ src/VUMeter/VUMeter.ino
@@ -8,22 +8,40 @@ float measuredAverageVoltage = 1.76;
 uint16_t offsetMidpoint = (measuredAverageVoltage / 5) * 1024;
 
 
-uint16_t iir(uint16_t oldData, uint16_t newData, float strength);
 
 void setup()
 {
 	Serial.begin(115200);
 	resetRaw();
+	inputRaw[0][0] = 0;
 }
 
 
 void loop()
 {
+/*
 	Serial.print(inputRaw[0][1]);
-	Serial.print(",");
+	Serial.print(" , ");
 	Serial.print(inputRaw[0][0]);
-	Serial.print(" = ");
+	Serial.print(" , ");
 	Serial.println(amplitudeValue(analogRead(A0)));
+	*/
+
+	uint16_t input = analogRead(A0);
+	if (input > offsetMidpoint)
+		inputRaw[0][0] = iir(inputRaw[0][0],input-offsetMidpoint,0.5);
+	//Serial.print(input);
+	//Serial.print(" : ");
+	Serial.print(inputRaw[0][0]);
+	Serial.print(" : ");
+	int scale = inputRaw[0][0] / 2;
+	Serial.print(scale);
+	Serial.print(" : ");
+
+	for(int i = 0; i < scale ; ++i )
+		Serial.print("=");
+
+	Serial.println();
 }
 
 void resetRaw()
@@ -41,29 +59,30 @@ void resetRaw()
 
 uint16_t amplitudeValue(uint16_t input)
 {
-	if (input < inputRaw[0][0]) {
+	if (input < offsetMidpoint) {
 		inputRaw[0][0] = iir(inputRaw[0][0],input,0.2);	
-	}else if(inputRaw[0][0] != offsetMidpoint) {
-		inputRawReady[0][0] = true;
 	}
 
-	if (input > inputRaw[0][1]) {
+	if (input > offsetMidpoint) {
 		inputRaw[0][1] = iir(inputRaw[0][1],input,0.2);	
-	}else if(inputRaw[0][1] != offsetMidpoint) {
-		inputRawReady[0][1] = true;
 	}
 
-	
-	if (inputRawReady[0][1] && inputRawReady[0][0]) {
-		uint16_t value = inputRaw[0][1] - inputRaw[0][0];
-		resetRaw();
-		return value;
-	}else{
-		return 0;
-	}
+	uint16_t value = inputRaw[0][1] - inputRaw[0][0];
+	return value;
 }
 
-uint16_t iir(uint16_t oldData, uint16_t newData, float strength)
+uint16_t iir(int32_t oldData, int32_t newData, float strength)
 {
-	return oldData += strength * (oldData - newData);
+/*	Serial.print("Old Data: ");
+	Serial.println(oldData);
+	Serial.print("New Data: ");
+	Serial.println(newData);
+	Serial.print("Difference: ");
+	Serial.println(newData-oldData);
+	Serial.print("Change: ");
+	Serial.println(strength*(newData-oldData));
+	Serial.print("New Value: ");
+	Serial.println(oldData + strength*(newData-oldData));*/
+
+	return oldData + strength * (newData - oldData);
 }
